b58cb9ee93bffe3d816308ac2424a7073983ec62
ayush-qubit <ayushsharma1198@gmail.com>
2020-08-02 21:55:12 +0530

    commit

---
+++ COMPILER.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long int ll;
+string str;
+ll n,len;
+void solution(string str){
+    stack<char> s;
+    len=0;
+    for(ll i=0;i<n;i++){
+        if(str[i]=='<'){
+            s.push(str[i]);
+        }
+        else{
+            if(s.empty() && str[i]=='>'){
+                break;
+            }
+            else{
+                s.pop();
+                len=i+1;
+            }
+        }
+    }
+    if(!s.empty()){
+        cout<<0;
+    }
+    else{
+        cout<<len;
+    }
+}
+int main(){
+    ll t;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cin>>str;
+        solution(str);
+        cout<<endl;
+    }
+}
